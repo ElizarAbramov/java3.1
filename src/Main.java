ddcec442c3a1f776a201851f98751a9bdc0d91ba
ElizarAbramov <88653410+ElizarAbramov@users.noreply.github.com>
2021-10-01 17:26:23 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,10 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
 
-
         int bonus = service.calculate(10_000);
         System.out.println(bonus);
 
-
         int bonusAnother = service.calculate(50_000);
         System.out.println(bonusAnother);
-
     }
 }
